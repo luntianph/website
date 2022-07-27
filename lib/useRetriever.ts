55bcc3dd1c1b22805194069993b157b314366054
Jared Sy <jaredblase@gmail.com>
2022-07-27 18:39:10 +0800

    add(lib): retriever

---
+++ lib/useRetriever.ts
@@ -0,0 +1,24 @@
+import useSWR from 'swr'
+import app from './axios-config'
+
+export function useRetrieverWithFallback<T>(key: string | null, fallback: T) {
+	const { data, error, mutate } = useSWR(key, url => app.get<T>(url).then(res => res.data))
+
+	return {
+		data: data == undefined ? fallback : data,
+		isLoading: !data && !error,
+		isError: !!error,
+		mutate
+	}
+}
+
+export default function useRetriever<T>(key: string | null) {
+	const { data, error, mutate } = useSWR(key, url => app.get<T>(url).then(res => res.data))
+
+	return {
+		data: data,
+		isLoading: !data && !error,
+		isError: !!error,
+		mutate
+	}
+}
