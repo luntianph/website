70bb6f21af7920fdb12c491d30fbdbe489bd3fd0
Jared Sy <jaredblase@gmail.com>
2022-08-14 22:05:28 +0800

    add: messenger URL

--- lib/urls.ts
+++ lib/urls.ts
@@ -1,4 +1,5 @@
 export const FACEBOOK_URL = 'https://www.facebook.com/luntianofficialph'
+export const MESSENGER_URL = 'https://m.me/luntianofficialph'
 export const TIKTOK_URL = 'https://www.tiktok.com/@luntianofficialph'
 export const IG_URL = 'https://www.instagram.com/luntianofficialph/'
 export const SHOPEE_URL = 'https://shopee.ph/luntiancompanyph'
@@ -6,6 +7,7 @@ export const CONTACT_NUMBER = '09175782804'
 
 const URLs =  {
 	facebook: FACEBOOK_URL,
+	messenger: MESSENGER_URL,
 	tiktok: TIKTOK_URL,
 	ig: IG_URL,
 	shopee: SHOPEE_URL,
