67df1209bfa90765692dfec1b7d08085361f8ba3
Jared Sy <jaredblase@gmail.com>
2022-08-21 18:01:24 +0800

    add: toast axios error

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,3 +1,17 @@
+import axios from 'axios'
+import { toast } from 'react-toastify'
+import { toastErrorConfig } from './toast-defaults'
+
 export function matchPath(pathname: string, route: string, isHome?: boolean) {
 	return isHome ? pathname === route : pathname.startsWith(route)
+}
+
+/**
+ * Used to show the error message in a toast
+ * @param error - an error object of unknown type
+ */
+export function toastAxiosError(error: unknown) {
+	if (axios.isAxiosError(error)) {
+		toast.error(error.message, toastErrorConfig)
+	}
 }
