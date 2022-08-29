7fc93b7fca1e048f1c95158472c48b7f722154a9
Jared Sy <jaredblase@gmail.com>
2022-08-30 02:05:04 +0800

    add: add product feature

---
+++ pages/products/add.tsx
@@ -0,0 +1,34 @@
+import ProductForm from '@components/forms/product-form'
+import app from '@lib/axios-config'
+import { toastSuccess } from '@lib/toast-defaults'
+import { toastAxiosError } from '@lib/utils'
+import { ProductSchema } from '@models/product'
+import { NextPage } from 'next'
+import { useRouter } from 'next/router'
+import { useState } from 'react'
+
+const Page: NextPage = () => {
+	const { push } = useRouter()
+	const [isLoading, setIsLoading] = useState(false)
+
+	async function onSubmit(data: ProductSchema) {
+		setIsLoading(true)
+		try {
+			await app.post(`/api/products`, data)
+			push('/products')
+			toastSuccess('Product was successfully added!')
+		} catch (err) {
+			toastAxiosError(err)
+		}
+		setIsLoading(false)
+	}
+
+	return (
+		<div className="max-w-2xl mx-auto w-full">
+			<h2 className="text-3xl font-normal mt-8 mb-6 max-w">Add Product</h2>
+			<ProductForm onSubmit={onSubmit} className="w-full mb-20" isLoading={isLoading} />
+		</div>
+	)
+}
+
+export default Page
--- pages/products/index.tsx
+++ pages/products/index.tsx
@@ -4,25 +4,57 @@ import Head from 'next/head'
 import Image from 'next/image'
 import Link from 'next/link'
 import Products from '@models/product'
+import { useSession } from 'next-auth/react'
+import { TrashIcon } from '@heroicons/react/24/outline'
+import { useCallback } from 'react'
+import app from '@lib/axios-config'
+import { useRouter } from 'next/router'
+import { toastAxiosError } from '@lib/utils'
 
 const ProductsPage: NextPage<InferGetStaticPropsType<typeof getStaticProps>> = ({ products }) => {
+	const { status } = useSession()
+	const { reload } = useRouter()
+
+	const handleDelete = useCallback(async (id: string) => {
+		try {
+			await app.delete(`/api/products/${id}`)
+			reload()
+		} catch (err) {
+			toastAxiosError(err)
+		}
+	}, [reload])
+
 	return (
 		<div className="grid gap-x-4 gap-y-8 mt-10 mb-20 sm:grid-cols-2 px-4 container mx-auto max-w-3xl">
 			<Head>
 				<title>Luntian | Products</title>
 			</Head>
+			{status == 'authenticated' &&
+				<div className="col-span-full justify-self-end">
+					<Link href="/products/add">
+						<a className="btn green px-4 rounded-sm">
+							Add Products
+						</a>
+					</Link>
+				</div>
+			}
 			{products.map(p =>
 				<Link key={p._id} href={`/products/${p._id}`}>
 					<a className="cursor-pointer">
 						<div className="relative aspect-square w-full">
+							<div className="p-1 bg-gray-700 bg-opacity-80 absolute right-2 top-2 z-10 hover:bg-gray-800"
+								onClick={e => { e.preventDefault(); handleDelete(p._id) }}>
+								<TrashIcon className="aspect-square w-5 text-white" />
+							</div>
 							<Image src={p.images[0]} alt="An image" layout="fill" />
 						</div>
 						<h3 className="text-[#79834c] font-bold">{p.name}</h3>
 						<p className="text-sm text-gray-600">₱{p.price.toFixed(2)}</p>
 					</a>
 				</Link>
-			)}
-		</div>
+			)
+			}
+		</div >
 	)
 }
 
