a0cf8d9c19fd40c3a36ada1c4b21a3204981daa6
Jared Sy <jaredblase@gmail.com>
2022-09-01 13:43:19 +0800

    fix: material schema

--- models/material.ts
+++ models/material.ts
@@ -3,24 +3,29 @@ import * as yup from 'yup'
 
 export const materialSchema = yup.object({
 	name: yup.string().trim().required(),
-	items: yup.array().of(yup.lazy((item) => {
-		if (typeof item == 'string') {
-			return yup.string().required()
-		}
-
-		return yup.array().of(yup.string()).min(1).required()
-	}) as unknown as yup.AnySchema<string | string[]>).min(1).required()
+	items:
+		yup.array().of(
+			yup.array().of(
+				yup.lazy((item) => {
+					if (typeof item == 'string') return yup.string().required()
+					return yup.array().of(yup.string().required()).min(1).required()
+				}) as unknown as yup.AnySchema<string | string[]>
+			).length(2).required()
+		).min(1).required()
 })
 
-export interface IMaterial {
-	_id: ObjectId
+export interface MaterialSchema {
 	name: string
-	items: [string, string[]]
+	items: [string, string[]][]
+}
+
+export interface IMaterial extends MaterialSchema {
+	_id: ObjectId
 }
 
 const schema = new Schema<IMaterial>({
 	name: { type: String, required: true, trim: true },
-	items: { type: [String, [String]], required: true, minlength: 1 }
-})
+	items: { type: [], required: true, minlength: 1 }
+}, { versionKey: false })
 
 export default models?.Material as Model<Document & IMaterial> || model<IMaterial>('Material', schema, 'materials')
