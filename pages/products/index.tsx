dd151d4021dd0bd2c79ccee2d64d8ba36badc009
Jared Sy <jaredblase@gmail.com>
2022-09-01 15:53:08 +0800

    add: confirmation modal for deleting products

--- pages/products/index.tsx
+++ pages/products/index.tsx
@@ -6,62 +6,72 @@ import Link from 'next/link'
 import Products from '@models/product'
 import { useSession } from 'next-auth/react'
 import { TrashIcon } from '@heroicons/react/24/outline'
-import { useCallback } from 'react'
+import { useCallback, useState } from 'react'
 import app from '@lib/axios-config'
 import { useRouter } from 'next/router'
 import { toastAxiosError } from '@lib/utils'
+import ConfirmationModal from '@components/modal/confirmation-modal'
 
 const ProductsPage: NextPage<InferGetStaticPropsType<typeof getStaticProps>> = ({ products }) => {
 	const { status } = useSession()
 	const { reload } = useRouter()
+	const [selection, setSelection] = useState('')
 
-	const handleDelete = useCallback(async (id: string) => {
+	async function handleDelete() {
 		try {
-			await app.delete(`/api/products/${id}`)
+			await app.delete(`/api/products/${selection}`)
 			reload()
 		} catch (err) {
 			toastAxiosError(err)
 		}
-	}, [reload])
+	}
 
 	return (
-		<div className="grid gap-x-4 gap-y-8 mt-10 mb-20 sm:grid-cols-2 px-4 container mx-auto max-w-3xl">
-			<Head>
-				<title>Luntian | Products</title>
-			</Head>
-			{status == 'authenticated' &&
-				<div className="col-span-full justify-self-end space-x-2">
-					<Link href="/products/add">
-						<a className="btn green px-3 py-2 rounded-sm">
-							Add Products
+		<>
+			<ConfirmationModal
+				isOpen={selection != ''}
+				onClose={() => setSelection('')}
+				onActionClick={handleDelete}
+				message="Delete product?"
+			/>
+			<div className="grid gap-x-4 gap-y-8 mt-10 mb-20 sm:grid-cols-2 px-4 container mx-auto max-w-3xl">
+				<Head>
+					<title>Luntian | Products</title>
+				</Head>
+				{status == 'authenticated' &&
+					<div className="col-span-full justify-self-end space-x-2">
+						<Link href="/products/add">
+							<a className="btn green px-3 py-2 rounded-sm">
+								Add Products
+							</a>
+						</Link>
+						<Link href="/materials">
+							<a className="btn green px-3 py-2 rounded-sm">
+								Edit Materials
+							</a>
+						</Link>
+					</div>
+				}
+				{products.map(p =>
+					<Link key={p._id} href={`/products/${p._id}`}>
+						<a className="cursor-pointer">
+							<div className="relative aspect-square w-full">
+								{status == 'authenticated' &&
+									<div className="p-1 bg-gray-700 bg-opacity-80 absolute right-2 top-2 z-10 hover:bg-gray-800"
+										onClick={e => { e.preventDefault(); setSelection(p._id) }}>
+										<TrashIcon className="aspect-square w-5 text-white" />
+									</div>
+								}
+								<Image src={p.images[0]} alt="An image" layout="fill" />
+							</div>
+							<h3 className="text-[#79834c] font-bold">{p.name}</h3>
+							<p className="text-sm text-gray-600">₱{p.price.toFixed(2)}</p>
 						</a>
 					</Link>
-					<Link href="/materials">
-						<a className="btn green px-3 py-2 rounded-sm">
-							Edit Materials
-						</a>
-					</Link>
-				</div>
-			}
-			{products.map(p =>
-				<Link key={p._id} href={`/products/${p._id}`}>
-					<a className="cursor-pointer">
-						<div className="relative aspect-square w-full">
-							{status == 'authenticated' &&
-								<div className="p-1 bg-gray-700 bg-opacity-80 absolute right-2 top-2 z-10 hover:bg-gray-800"
-									onClick={e => { e.preventDefault(); handleDelete(p._id) }}>
-									<TrashIcon className="aspect-square w-5 text-white" />
-								</div>
-							}
-							<Image src={p.images[0]} alt="An image" layout="fill" />
-						</div>
-						<h3 className="text-[#79834c] font-bold">{p.name}</h3>
-						<p className="text-sm text-gray-600">₱{p.price.toFixed(2)}</p>
-					</a>
-				</Link>
-			)
-			}
-		</div >
+				)
+				}
+			</div >
+		</>
 	)
 }
 
