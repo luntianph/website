edc8abc3337741173e4e5a83a50d0495c5151351
Jared <jaredblase@gmail.com>
2022-12-04 00:26:00 +0800

    fix: ui bugs

--- components/team-member/team-member.tsx
+++ components/team-member/team-member.tsx
@@ -8,13 +8,11 @@ type TeamMemberProps = {
 
 export function TeamMember({ name, position, src }: TeamMemberProps) {
 	return (
-		<div className="flex justify-center">
-			<div className="bg-green-200">
-				<Image src={src} alt={`Picture of ${name}`} width={402} height={336} className="w-full" />
-				<div className="px-4 py-6">
-					<p className="text-xl text-center mb-1">{name}</p>
-					<p className="text-xs font-light text-center">{position}</p>
-				</div>
+		<div className="bg-green-200">
+			<Image src={src} alt={`Picture of ${name}`} width={402} height={336} className="w-full" />
+			<div className="px-4 py-6">
+				<p className="text-xl text-center mb-1">{name}</p>
+				<p className="text-xs font-light text-center">{position}</p>
 			</div>
 		</div>
 	)
